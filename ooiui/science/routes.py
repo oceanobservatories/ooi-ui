92009072d05a8fe0fec3da46326c17f90d8ac840
birdage <abird@asascience.com>
2014-12-05 13:43:09 -0500

    initial commit for new plotting tool

--- ooiui/science/routes.py
+++ ooiui/science/routes.py
@@ -11,6 +11,10 @@ from ooiui.config import TABLEDAP, SERVICES_URL, DEBUG
 import requests
 import os
 import json
+from datetime import datetime,timedelta
+import time
+import numpy as np
+import math
 
 @app.route('/pioneer/')
 def pioneer():
@@ -45,11 +49,120 @@ def getJsonData(instrument,start_date,end_date,parameters):
 
     r = requests.get(url);     
     if (r.status_code != 500): 
-        r = r.json()["table"]
-        r = json.dumps(r,indent=4)
+        data = r.json()['table']
+        return_json = ""    
+        try:                        
+            print "re-org-data"
+            trans_data = np.transpose(data['rows'])            
+            colIdx = -1    
+            colList = []  #minus time
+            unitList = []  #minus time
+            for i, col in enumerate(data['columnNames']):
+                if col == "time":
+                    colIdx = i                    
+                else:
+                    unitList.append(data['columnUnits'][i])    
+                    colList.append(col)
+            colNames = data['columnNames']      
+
+            #create array of the data dates
+            date_array_int = []
+            date_array = []
+            for d in (trans_data[colIdx]):        
+                date_object = datetime.strptime(d, "%Y-%m-%dT%H:%M:%SZ")
+                date_array.append(date_object)
+                date_array_int.append(time.mktime(date_object.timetuple()) * 1000)
+
+
+            date_array_int = np.array(date_array_int)
+            date_array = np.array(date_array)
+            
+            #get start and end of data
+            end_date = date_array[-1]
+            start_date = date_array[0]
+
+            #calc the diff between the start and end dates
+            diff_in_days = abs(start_date - end_date)
+            dd = divmod(diff_in_days.days * 86400 + diff_in_days.seconds, 60)
+            #convert minutes to hours
+            diff_in_hours = int(math.ceil(dd[0]*0.0166667))
+            print "hours diff between start and end:", diff_in_hours
+
+            #get the start date in miliseconds
+            st = time.mktime(start_date.timetuple()) * 1000
+            ed = time.mktime(end_date.timetuple()) * 1000
+
+            dt_data = []
+            for i in xrange(0,diff_in_hours):   #should be a while loop
+                st_datetime = start_date + timedelta(hours=i)
+                ed_datetime = start_date + timedelta(hours=i+1)                    
+                middle_datetime = st_datetime + timedelta(minutes=30)                
+
+                #create datetime from generate vals
+                st_val = time.mktime(st_datetime.timetuple()) * 1000
+                ed_val = time.mktime(ed_datetime.timetuple()) * 1000
+
+                #index of values
+                st_idx = (find_nearest(date_array_int, st_val))
+                ed_idx = (find_nearest(date_array_int, ed_val))                
+
+                #get the int values
+                st_time_int = date_array_int[st_idx]
+                ed_time_int = date_array_int[ed_idx]
+
+                #get the difference between the date times
+                diff_in_days = abs((date_array[st_idx] - date_array[ed_idx]))
+                dd = divmod(diff_in_days.days * 86400 + diff_in_days.seconds, 60)
+                #convert minutes to hours
+                diff_in_hours_data = dd[0]*0.0166667
+
+                data_row = {}
+                for j,col in enumerate(data['columnNames']):                    
+                    if st_idx < ed_idx:                        
+                        if not col == "time":                                                      
+                            val_row = []
+                            val_min = np.min(np.array(trans_data[j][st_idx:ed_idx],np.float))
+                            val_max = np.max(np.array(trans_data[j][st_idx:ed_idx],np.float))
+                            val_mean = np.mean(np.array(trans_data[j][st_idx:ed_idx],np.float))
+                            val_row.append(val_min)
+                            val_row.append(val_mean)
+                            val_row.append(val_max)
+                            data_row[col] = val_row
+                        else:                         
+                            data_row['time'] = (str(middle_datetime))
+
+                        #print data_row,diff_in_hours_data
+
+                #add the data only if there is contents
+                if len(data_row)>0:
+                    dt_data.append(data_row)
+
+
+            return_json = { "total_hours_start_end":diff_in_hours,
+                            "data_start_date":str(start_date),
+                            "data_end_date": str(end_date),
+                            "data":dt_data,
+                            "timezone":"UTC",
+                            "fields":colList,
+                            "time_note":"time is middle of the timespan, time span is in hours",
+                            "time_span":"1",
+                            "variables":["min","mean","max"],
+                            "units":unitList}
+
+        except Exception, e:
+            return_json = "{FAIL}"
+            print "FAIL!",str(e)
+
+        r = json.dumps(return_json,indent=4)
         return r
     else:
         raise Exception("data request error") 
+
+
+def find_nearest(array,value):
+    idx = (np.abs(array-value)).argmin()
+    return idx
+
 
 @app.route('/get_time_coverage/<ref>/<stream>')
 def get_time_coverage(ref, stream):
--- ooiui/science/static/js/ooi/plot.js
+++ ooiui/science/static/js/ooi/plot.js
@@ -1,27 +1,32 @@
 function enablePlotting(){
-	$('.bottom-div').show();
+    $('.bottom-div').show();
 }
 
 
 function disablePlotting(){
-	$('.bottom-div').hide();
-	//$( "#plotting" ).empty();     
+    $('.bottom-div').hide();
+    //$( "#plotting" ).empty();     
 }
 
 function updateData(array,platform,instrument){
-    console.log("updateData");
-	enablePlotting();
-	$( "#plotting" ).empty();  
+    //console.log("updateData");
+    enablePlotting();
+    $( '#plotting' ).hide();
+    $( "#plotting" ).empty();  
+    //get list of selected getSelectedVariables
 
-	$( '#plotting' ).hide();
-	$('#processing-icon').show();
-
-    getData(array,platform,instrument,$('#stream-select option:selected').text(), $( "#variable-select option:selected").attr("value"));
+    vars = getSelectedVariables();  
+    if (vars.length > 0){       
+        $('#processing-icon').show();
+        getData(array,platform,instrument,$('#stream-select option:selected').text(), vars);
+    }else{
+        $('#plotting').show();
+        $('#processing-icon').hide();
+    }
 
 }
 
-function getTimeCoverage(array,platform,instrument) {
-    console.log("getTimeCoverage");
+function getTimeCoverage(array,platform,instrument) {    
     var instrument = $( "#currentInstrument" ).text();      
     var stream = $('#stream-select option:selected').text();
 
@@ -31,54 +36,54 @@ function getTimeCoverage(array,platform,instrument) {
 
 
     $.getJSON(url, function( data ) {  
-        console.log("This is the data");
-        console.log(data);
         var end_date = new Date(data['time_coverage_end']);
         var start_date = new Date(data['time_coverage_end']);
         start_date = start_date.setDate(start_date.getDate() - 14);
 
         $('#datetimepicker_from').data("DateTimePicker").setDate(new Date(start_date))
         $('#datetimepicker_to').data("DateTimePicker").setDate(new Date(end_date))
-        console.log(start_date);
-        console.log(end_date);
     });
 
 }
 
 var getDataXHR = null;
 
-function getData(array,platform,instrument,stream,variable){
-	//get the from date
-	from_dt = ($('#datetimepicker_from').data("DateTimePicker").getDate().toISOString());
-	to_dt   = ($('#datetimepicker_to').data("DateTimePicker").getDate().toISOString());	
+function getData(array,platform,instrument,stream,variables){
+    //get the from date
+    from_dt = ($('#datetimepicker_from').data("DateTimePicker").getDate().toISOString());
+    to_dt   = ($('#datetimepicker_to').data("DateTimePicker").getDate().toISOString()); 
 
-	url  = "/getdata/?"
-	url += "dataset_id="+instrument + '_' + stream
-	url += "&startdate="+from_dt
-	url += "&enddate="+to_dt
-	url += "&variables="+variable
+    variables = variables.join()
 
-	console.log(url)
+    url  = "/getdata/?"
+    url += "dataset_id="+instrument + '_' + stream
+    url += "&startdate="+from_dt
+    url += "&enddate="+to_dt
+    url += "&variables="+variables
+
+    console.log(url)
     if(getDataXHR != null) {
         console.log("Aborting other XHR request");
         getDataXHR.abort();
         getDataXHR = null;
     }
 
-	getDataXHR = $.ajax({		
-		type: 'GET',
+    getDataXHR = $.ajax({       
+        type: 'GET',
         url: url,  
         dataType: 'json',
         contentType: "application/json; charset=utf-8"
     })
     .success(function(data){
-    	$('#plotting').show();
-		$('#processing-icon').hide();
-    	makePlot(array,platform,instrument,variable,data);
+        $('#plotting').show();
+        $('#processing-icon').hide();
+        makePlot(array,platform,instrument,variables,data);
     })
-	.error(function(xhr, status, error) {
-		//var err = eval("(" + xhr.responseText + ")");
-    	console.log( "error:",error );
+    .error(function(xhr, status, error) {
+        //var err = eval("(" + xhr.responseText + ")");
+        console.log( "error:",error );
+        $('#plotting').show();
+        $('#processing-icon').hide();
     });
 }
 
@@ -86,63 +91,110 @@ function getRowData(rows){
 
 }
 
-function makePlot(array,platform,instrument,variable,data){
-	console.log(data)
-	rowdata = data["rows"]
-	units = data["columnUnits"][1]
+function makePlot(array,platform,instrument,variable,dataObj){
+    console.log("DATA:",dataObj)
+    var_list = dataObj['fields']
+    data = dataObj['data']
+    units = dataObj['units']
 
-	var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
+    spacing = 65
+
+    var margin = {top: 20, right: 20, bottom: 30, left: (var_list.length*spacing)},
+    width = 800 - margin.left - margin.right,
     height = 300 - margin.top - margin.bottom;
 
-	var parseDate = d3.time.format("%d-%b-%y").parse;
+    var parseDate = d3.time.format("%d-%b-%y").parse;
 
-	var x = d3.time.scale()
-	    .range([0, width]);
+    yScaleList = []
+    yAxisList = []
+    for (var i = 0; i < var_list.length; i++) {
+        var y = d3.scale.linear().range([height, 0]);
+        y.domain(d3.extent(data, function(d) { return d[var_list[i]][2]; }));
+        var yAxis = d3.svg.axis().scale(y).orient("left");
 
-	var y = d3.scale.linear()
-	    .range([height, 0]);
+        yScaleList.push(y)
+        yAxisList.push(yAxis)
+    };
+    
+    var x = d3.time.scale().range([0, width]);
+    x.domain(d3.extent(data, function(d) { 
+        d = d['time']
+        return new Date(d);
+    }));
 
-	var xAxis = d3.svg.axis()
-	    .scale(x)
-	    .orient("bottom");
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");      
 
-	var yAxis = d3.svg.axis()
-	    .scale(y)
-	    .orient("left");
+    var svg = d3.select("#plotting").append("svg")
+        .attr("id","plotview")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	var line = d3.svg.line()
-	    .x(function(d) { return x(new Date(d[0])); })
-	    .y(function(d) { return y(d[1]); });
+    
 
-	var svg = d3.select("#plotting").append("svg")
-	    .attr("id","plotview")
-	    .attr("width", width + margin.left + margin.right)
-	    .attr("height", height + margin.top + margin.bottom)
-	  .append("g")
-	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
 
-	x.domain(d3.extent(rowdata, function(d) { return new Date(d[0]);}));
-	y.domain(d3.extent(rowdata, function(d) { return d[1]; }));
+    baseY = 3
+    var p=d3.scale.category10();
+    var colorVals=p.range();
+    for (var i = 0; i < var_list.length; i++) {
+        svg.append("g")
+        .attr("class", "y axis")        
+        .attr("transform", "translate("+(baseY-(i*spacing))+",0)")   //move the axis along a bit....
+        .style("fill", function (d) { return colorVals[i];})         
+        .style("stroke", function (d) { return colorVals[i];})     
+        .attr("data-legend",function() { return var_list[i]})
+        .call(yAxisList[i])       
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", baseY)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text(units[i]);
+    }
+    
 
-	  svg.append("g")
-	      .attr("class", "x axis")
-	      .attr("transform", "translate(0," + height + ")")
-	      .call(xAxis);
+    dataLines = svg.selectAll("line").data(data).enter()
+    dataCircles = svg.selectAll("circle").data(data).enter()    
+    
+    for (var i = 0; i < var_list.length; i++) {
+       
+        /*
+        dataLines.append("line")
+            .attr("y1", function(d, i) { return y(d[1]) })
+            .attr("x1", function(d, i) { return x(new Date(d[0])); })
+            .attr("y2", function(d, i) { return y(d[3]) })
+            .attr("x2", function(d, i) { return x(new Date(d[0])); })               
+            .attr("class","dataline");
+        */
 
-	  svg.append("g")
-	      .attr("class", "y axis")
-	      .call(yAxis)	      
-	    .append("text")
-	      .attr("transform", "rotate(-90)")
-	      .attr("y", 6)
-	      .attr("dy", ".71em")
-	      .style("text-anchor", "end")
-	      .text(units);
+        dataCircles.append("circle")
+            .attr("cy", function(d) {   
+                d = d[var_list[i]]             
+                return yScaleList[i](d[1])
+            })
+            .attr("cx", function(d) {                
+                d = (d['time']).toString();                
+                return x(new Date(d)); 
+            })
+            .attr("r", function(d) { return 3 })   
+            .attr("class", "datacircle")   
+            .style("fill",function (d) { 
+                return colorVals[i]; 
+            });     
+            
+    };   
 
-	  svg.append("path")
-	      .datum(rowdata)
-	      .attr("class", "line")
-	      .attr("d", line);
+    legend = svg.append("g")
+        .attr("class","legend")
+        .attr("transform","translate(50,30)")
+        .style("font-size","12px")
+        .call(d3.legend) 
 
 }
