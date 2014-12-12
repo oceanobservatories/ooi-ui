429140b3c68c35c6f3acd3bf63c4e647f9f431bd
Matt Campbell <mcampbell@asascience.com>
2014-12-11 22:16:13 -0500

    further refinement of get_api list, made something to test with.

--- ooiui/common/static/js/ooiservices.js
+++ ooiui/common/static/js/ooiservices.js
@@ -5,19 +5,22 @@ Requires current service running from ooi-ui-service
 */
 /* TODO: Externalize this */
 var BASE_URL = 'http://localhost:4000/';
+var jsonMimeType = "application/json;charset=UTF-8";
 //Helper function to do the GET
 function _getForm(url_val, callback) {
     jQuery.ajax({
-         type: "GET",
          url: url_val,
-         contentType: "application/javascript; charset=utf-8",
-         dataType: "jsonp",
+         dataType: 'jsonp',
+         complete: function(jqXHR, status) {
+             console.debug(status);
+         },
          success: function (data) {
             result = JSON.stringify(data, null, 2);
             callback(result);
+            console.debug('raw: ' + result);
          },
-         error: function (jqXHR, status) {
-            console.error(url_val, status, err.toString());
+         error: function (jqXHR, status, error) {
+            console.error(url_val, error);
          }
     });
 }
@@ -25,14 +28,15 @@ function _getForm(url_val, callback) {
 function _buildList(json_string) {
     var data = JSON.parse(json_string);
     //Build a simple json dictionary of only the ids
-    var new_json = '[{';
+    var new_json = '[';
     for (var i=0; i<data.length; i++) {
-        new_json += '"id" : "' + data[i].id + '" ,';
+        new_json += '{ "id" : "' + data[i].id + '" },';
     }
     new_json = new_json.slice(0, -1);
     //Remove the trailing comma (,)
     new_json = new_json.substring(0, new_json.length-1) + "}]";
-    return new_json;
+    console.debug('reformed:' + new_json);
+    return JSON.stringify(new_json);
 }
 function getArray (array_id) {
     var url_val = BASE_URL + "arrays/" + array_id;
@@ -90,17 +94,17 @@ function getStreamList () {
         return _buildList(result);
     });
 }
-function getPlatformsAtArray (array_id) {
+function getPlatformListAtArray (array_id) {
     var url_val = BASE_URL + "platforms?array_id=" + array_id;
     _getForm(url_val, function(result) {
-        //Default behavior: return JSON.
-        return result;
+        //Return a list of platforms at a specified array
+        return _buildList(result);
     });
 }
-function getInstrumentsAtPlatform (platform_id) {
+function getInstrumentListAtPlatform (platform_id) {
    var url_val = BASE_URL + "instruments?platform_id=" + platform_id;
-   _getForm(url_val, function(result) {
-       //Default behavior: return JSON.
-        return result;
+    _getForm(url_val, function(result) {
+        //Return a list of instruments at specified platform
+        return _buildList(result);
     });
 }
