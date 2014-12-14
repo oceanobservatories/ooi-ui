ca4d5d104398ad306d51b0a8617d0d8202f8a79b
Matt <mjc@mjc-air-2.local>
2014-12-14 17:29:41 -0500

    BuildNavMenu added, creates a <li>.  Will expand list to be buttons

--- ooiui/common/static/js/ooi/api-GET.js
+++ ooiui/common/static/js/ooi/api-GET.js
@@ -5,38 +5,42 @@ Requires current service running from ooi-ui-service
 */
 /* TODO: Externalize this */
 var BASE_URL = 'http://localhost:4000/';
-var jsonMimeType = "application/json;charset=UTF-8";
-//Helper function to do the GET
+var glabal_result = "";
 function _getForm(url_val, callback) {
+    //Helper function to do the GET
     jQuery.ajax({
          url: url_val,
+         contentType: "application/javascript",
          dataType: 'jsonp',
          complete: function(jqXHR, status) {
-             console.debug(status);
+             //console.debug(status);
          },
          success: function (data) {
             result = JSON.stringify(data, null, 2);
-            callback(result);
-            console.debug('raw: ' + result);
+            setTimeout(function() { callback(result); }, 100);
+            //console.debug('raw: ' + result);
          },
          error: function (jqXHR, status, error) {
             console.error(url_val, error);
          }
     });
 }
-//Helper function for building lists.
 function _buildList(json_string) {
+    //Helper function for building lists.
     var data = JSON.parse(json_string);
+
     //Build a simple json dictionary of only the ids
     var new_json = '[';
     for (var i=0; i<data.length; i++) {
-        new_json += '{ "id" : "' + data[i].id + '" },';
+        new_json += '{ "id" : "' + data[i].id + '" , "name" : "' + data[i].description + '"},';
     }
     new_json = new_json.slice(0, -1);
     //Remove the trailing comma (,)
     new_json = new_json.substring(0, new_json.length-1) + "}]";
-    console.debug('reformed:' + new_json);
-    return JSON.stringify(new_json);
+
+    //console.debug('reformed:' + new_json);
+    //var new_json = JSON.stringify(new_json);
+    return new_json;
 }
 function getArray (array_id) {
     var url_val = BASE_URL + "arrays/" + array_id;
@@ -48,9 +52,11 @@ function getArray (array_id) {
 function getArrayList () {
     var url_val = BASE_URL + "arrays";
     _getForm(url_val, function(result) {
-        //Return a list of id
-        return _buildList(result);
+        //TODO: maybe find a better place to store this value.
+        window.name = result;
     });
+
+    return _buildList(name);
 }
 function getPlatform (platform_id) {
     var url_val = BASE_URL + "platforms/" + platform_id;
@@ -103,6 +109,13 @@ function getPlatformListAtArray (array_id) {
 }
 function getInstrumentListAtPlatform (platform_id) {
    var url_val = BASE_URL + "instruments?platform_id=" + platform_id;
+    _getForm(url_val, function(result) {
+        //Return a list of instruments at specified platform
+        return _buildList(result);
+    });
+}
+function getStreamListAtInstrument (stream_id) {
+   var url_val = BASE_URL + "streams?instrument_id=" + stream_id;
     _getForm(url_val, function(result) {
         //Return a list of instruments at specified platform
         return _buildList(result);
