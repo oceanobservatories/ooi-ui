6b57842a40a1449572f12409c9a3d75a2648da29
Matt <mjc@mjc-air-2.local>
2014-12-14 20:17:33 -0500

    callback hell?

--- ooiui/common/static/js/ooi/api-GET.js
+++ ooiui/common/static/js/ooi/api-GET.js
@@ -5,128 +5,116 @@ Requires current service running from ooi-ui-service
 */
 /* TODO: Externalize this */
 var BASE_URL = 'http://localhost:4000/';
+var global = null;
+// BEGIN INTERNAL FUNCTIONS */
 function _getForm(url_val, callback) {
-    //Helper function to do the GET
-    jQuery.ajax({
-         url: url_val,
-         contentType: "application/javascript",
-         dataType: 'jsonp',
-         complete: function(jqXHR, status) {
-             //console.debug(status);
-         },
-         success: function (data) {
-            result = JSON.stringify(data, null, 2);
-            setTimeout(function() { callback(result); }, 100);
-            //console.debug('raw: ' + result);
-         },
-         error: function (jqXHR, status, error) {
-            console.error(url_val, error);
-         }
-    });
-}
-function _buildList(json_string) {
-    //Helper function for building lists.
-    var data = JSON.parse(json_string);
-
-    //Build a simple json dictionary of only the ids
-    var new_json = '[';
-    for (var i=0; i<data.length; i++) {
-        new_json += '{ "id" : "' + data[i].id + '" , "name" : "' + data[i].description + '"},';
+        jQuery.ajax({
+             url: url_val,
+             contentType: "application/javascript",
+             dataType: 'jsonp',
+             complete: function(jqXHR, status) {
+                 //console.debug(status);
+             },
+             success: function (data) {
+                result = JSON.stringify(data, null, 2);
+                setTimeout(function() { callback(result); }, 100);
+                //console.debug('raw: ' + result);
+             },
+             error: function (jqXHR, status, error) {
+                console.error(url_val, error);
+             }
+        });
     }
-    new_json = new_json.slice(0, -1);
-    //Remove the trailing comma (,)
-    new_json = new_json.substring(0, new_json.length-1) + "}]";
+function _buildList(json_string) {
+        var data = JSON.parse(json_string);
 
-    //console.debug('reformed:' + new_json);
-    //var new_json = JSON.stringify(new_json);
-    return new_json;
-}
+        //Build a simple json dictionary of only the ids
+        var new_json = '[';
+        for (var i=0; i<data.length; i++) {
+            new_json += '{ "id" : "' + data[i].id + '" , "name" : "' + data[i].description + '"},';
+        }
+        new_json = new_json.slice(0, -1);
+        //Remove the trailing comma (,)
+        new_json = new_json.substring(0, new_json.length-1) + "}]";
+
+        //console.debug('reformed:' + new_json);
+        //var new_json = JSON.stringify(new_json);
+        return new_json;
+    }
+// END INTERNAL FUNCTIONS */
+// --------------------------- */
+// BEGIN OBJECT RETURN FUNCTIONS */
 function getArray (array_id) {
-    var url_val = BASE_URL + "arrays/" + array_id;
-    _getForm(url_val, function(result) {
-        //Default behavior: return JSON.
-        return result;
-    });
-}
-function getArrayList () {
+        var url_val = BASE_URL + "arrays/" + array_id;
+        _getForm(url_val, function(result) {
+            //Default behavior: return JSON.
+            return result;
+        });
+    }
+function getPlatform (platform_id) {
+        var url_val = BASE_URL + "platforms/" + platform_id;
+        _getForm(url_val, function(result) {
+            //Default behavior: return JSON.
+            return result;
+        });
+    }
+function getInstrument (instrument_id) {
+        var url_val = BASE_URL + "instruments/" + instrument_id;
+        _getForm(url_val, function(result) {
+            //Default behavior: return JSON.
+            return result;
+        });
+    }
+function getStream (stream_id) {
+        var url_val = BASE_URL + "streams/" + stream_id;
+        _getForm(url_val, function(result) {
+            //Default behavior: return JSON.
+            return result;
+        });
+    }
+// END OBJECT RETURN FUNCTIONS */
+// --------------------------- */
+// BEGIN LIST RETURN FUNCTIONS */
+function getArrayList (callback) {
     var url_val = BASE_URL + "arrays";
     _getForm(url_val, function(result) {
-        //TODO: maybe find a better place to store this value.
-        window.name = result;
-    });
-
-    return _buildList(name);
-}
-function getPlatform (platform_id) {
-    var url_val = BASE_URL + "platforms/" + platform_id;
-    _getForm(url_val, function(result) {
-        //Default behavior: return JSON.
-        return result;
+        callback(_buildList(result));
     });
 }
 function getPlatformList() {
     var url_val = BASE_URL + "platforms";
     _getForm(url_val, function(result) {
-        //TODO: maybe find a better place to store this value.
-        window.name = result;
-    });
-
-    return _buildList(name);
-}
-function getInstrument (instrument_id) {
-    var url_val = BASE_URL + "instruments/" + instrument_id;
-    _getForm(url_val, function(result) {
-        //Default behavior: return JSON.
-        return result;
+        callback(_buildList(result));
     });
 }
 function getInstrumentList () {
     var url_val = BASE_URL + "instruments";
     _getForm(url_val, function(result) {
-        //TODO: maybe find a better place to store this value.
-        window.name = result;
-    });
-
-    return _buildList(name);
-}
-function getStream (stream_id) {
-    var url_val = BASE_URL + "streams/" + stream_id;
-    _getForm(url_val, function(result) {
-        //Default behavior: return JSON.
-        return result;
+        callback(_buildList(result));
     });
 }
 function getStreamList () {
     var url_val = BASE_URL + "streams";
    _getForm(url_val, function(result) {
-        //TODO: maybe find a better place to store this value.
-        window.name = result;
+        callback(_buildList(result));
     });
-
-    return _buildList(name);
 }
 function getPlatformListAtArray (array_id) {
     var url_val = BASE_URL + "platforms?array_id=" + array_id;
-    _getForm(url_val, function(result) {
-        //TODO: maybe find a better place to store this value.
-        window.name = result;
+   _getForm(url_val, function(result) {
+        callback(_buildList(result));
     });
-
-    return _buildList(name);}
+}
 function getInstrumentListAtPlatform (platform_id) {
    var url_val = BASE_URL + "instruments?platform_id=" + platform_id;
     _getForm(url_val, function(result) {
-        //TODO: maybe find a better place to store this value.
-        window.name = result;
+        callback(_buildList(result));
     });
-
-    return _buildList(name);}
+}
 function getStreamListAtInstrument (stream_id) {
    var url_val = BASE_URL + "streams?instrument_id=" + stream_id;
      _getForm(url_val, function(result) {
-        //TODO: maybe find a better place to store this value.
-        window.name = result;
+        callback(_buildList(result));
     });
-
-    return _buildList(name);
 }
+// END LIST RETURN FUNCTIONS */
--- ooiui/common/static/js/ooi/build-nav-menu.js
+++ ooiui/common/static/js/ooi/build-nav-menu.js
@@ -6,17 +6,19 @@ Requires: ooiservice.js included
 
 function BuildNavMenu() {
     //TODO: Use the return to build a <li><a> of links
-    var data = getArrayList();
+    getArrayList(function(result) {
+        data = JSON.parse(result);
+        //Build a simple <li> element, where there is a onclick even.
+        var unordered_list = '<ul>';
+        for (var i=0; i<data.length; i++) {
+            unordered_list +=
+                '<li id="nav-item" onclick="">'
+                    + data[i].name + '( '
+                    + data[i].id +
+                ' )</li>';
+        }
+        document.write(unordered_list);
+    });
 
-    data = JSON.parse(data);
-    //Build a simple <li> element, where there is a onclick even.
-    var unordered_list = '<ul>';
-    for (var i=0; i<data.length; i++) {
-        unordered_list +=
-            '<li id="nav-item" onclick="">'
-                + data[i].name + '( '
-                + data[i].id +
-            ' )</li>';
-    }
-    document.write(unordered_list);
+
 }
