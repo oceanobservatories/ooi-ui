aed5498c3d312d5b1c4a8a4a31d64ba0d75d352c
birdage <abird@asascience.com>
2014-12-31 15:41:15 -0500

    remove bower and add travis test

--- bower_components/jasmine-jquery-matchers/webpack.config.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = require('./webpack.common')({
-  build: true,
-  lint: true
-});
--- bower_components/jasmine-jquery-matchers/webpack.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = require('./webpack.common')({
-  build: false,
-  lint: true,
-  cov: !!process.env.COV
-});
