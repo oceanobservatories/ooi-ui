51e8a4937226e6fcdb10156f9baa177868983ce1
Luke Campbell <luke.s.campbell@gmail.com>
2015-02-05 12:35:26 -0500

    Refactors to support CSRF
     - A Unique token can be generated by a JINJA request that creates a
       session token. That token should be submitted with any POST requests
       that don't require explicit user logins.

--- ooiui/core/app/__init__.py
+++ ooiui/core/app/__init__.py
@@ -7,9 +7,12 @@ Defines the application for the Science UI
 '''
 from flask import Flask
 from flask.ext.cache import Cache
-from ooiui.config import CACHE_TYPE
+from flask_environments import Environments
 
 app = Flask(__name__, static_url_path='', template_folder='../../templates', static_folder='../../static')
 
-cache = Cache(app, config={'CACHE_TYPE': CACHE_TYPE})
 
+env = Environments(app, default_env='DEVELOPMENT')
+env.from_yaml('ooiui/config/config.yml')
+
+cache = Cache(app, config={'CACHE_TYPE': app.config['CACHE_TYPE']})
--- ooiui/core/routes/common.py
+++ ooiui/core/routes/common.py
@@ -1,10 +1,10 @@
 from ooiui.core.app import app
-from flask import request, render_template, Response, jsonify
-from ooiui.config import SERVICES_URL
+from flask import request, render_template, Response, jsonify, session
 from werkzeug.exceptions import Unauthorized
 import requests
 import json
 import urllib
+from uuid import uuid4
 
 def get_login():
     token = request.cookies.get('ooiusertoken')
@@ -13,6 +13,13 @@ def get_login():
         raise Unauthorized(description="Invalid token", response=response)
     token = urllib.unquote(token).decode('utf8')
     return token
+
+def generate_csrf_token():
+    if '_csrf_token' not in session:
+        session['_csrf_token'] = uuid4().hex
+    return session['_csrf_token']
+
+app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
 @app.route('/signup')
 def user_signup():
@@ -48,36 +55,36 @@ def plots_demo():
 
 @app.route('/api/organization', methods=['GET'])
 def get_organization():
-    response = requests.get(SERVICES_URL + '/organization', params=request.args)
+    response = requests.get(app.config['SERVICES_URL'] + '/organization', params=request.args)
     return response.text, response.status_code
 
 @app.route('/api/organization/<int:id>', methods=['GET'])
 def get_organization_by_id(id):
-    response = requests.get(SERVICES_URL + '/organization/%s' % id, params=request.args)
+    response = requests.get(app.config['SERVICES_URL'] + '/organization/%s' % id, params=request.args)
     return response.text, response.status_code
 
 @app.route('/api/user', methods=['GET'])
 def get_users():
     token = get_login()
-    response = requests.get(SERVICES_URL + '/user', auth=(token, ''))
+    response = requests.get(app.config['SERVICES_URL'] + '/user', auth=(token, ''))
     return response.text, response.status_code
 
 @app.route('/api/current_user', methods=['GET'])
 def get_current_user():
     token = get_login()
-    response = requests.get(SERVICES_URL + '/current_user', auth=(token, ''))
+    response = requests.get(app.config['SERVICES_URL'] + '/current_user', auth=(token, ''))
     return response.text, response.status_code
 
 @app.route('/api/user/<int:id>', methods=['GET'])
 def get_user_by_id(id):
     token = get_login()
-    response = requests.get(SERVICES_URL + '/user/%s' % id, auth=(token, ''))
+    response = requests.get(app.config['SERVICES_URL'] + '/user/%s' % id, auth=(token, ''))
     return response.text, response.status_code
 
 @app.route('/api/user/<int:id>', methods=['PUT'])
 def put_user(id):
     token = get_login()
-    response = requests.put(SERVICES_URL + '/user/%s' % id, auth=(token, ''), data=request.data)
+    response = requests.put(app.config['SERVICES_URL'] + '/user/%s' % id, auth=(token, ''), data=request.data)
     return response.text, response.status_code
 
 @app.route('/api/user', methods=['POST'])
@@ -86,20 +93,23 @@ def submit_user():
     '''
     Acts as a pass-thru proxy to to the services
     '''
-    token = get_login()
-    response = requests.post(SERVICES_URL + '/user', auth=(token, ''), data=request.data)
+    csrf_token = session.pop('_csrf_token', None)
+    data = json.loads(request.data)
+    if not csrf_token or csrf_token != data['_csrf_token']:
+        return jsonify(error="CSRF Error"), 401
+    api_key = app.config['UI_API_KEY']
+    response = requests.post(app.config['SERVICES_URL'] + '/user', headers={'X-Csrf-Token' : api_key}, data=request.data)
     return response.text, response.status_code
 
 @app.route('/api/user_scope', methods=['GET'])
 def get_user_scopes():
     token = get_login()
-    response = requests.get(SERVICES_URL + '/user_scopes', auth=(token, ''))
+    response = requests.get(app.config['SERVICES_URL'] + '/user_scopes', auth=(token, ''))
     return response.text, response.status_code
 
 @app.route('/user_roles')
 def user_roles():
-    token = get_login()
-    resp = requests.get(SERVICES_URL + '/user_roles', auth=(token,''))
+    resp = requests.get(app.config['SERVICES_URL'] + '/user_roles')
     return resp.text, resp.status_code
 
 @app.route('/api/login', methods=['POST'])
@@ -107,7 +117,7 @@ def login():
     local_context = json.loads(request.data)
     username = local_context['login']
     password = local_context['password']
-    response = requests.get(SERVICES_URL + '/token', auth=(username, password))
+    response = requests.get(app.config['SERVICES_URL'] + '/token', auth=(username, password))
     return response.text, response.status_code
 
 
@@ -119,51 +129,51 @@ def login():
 
 @app.route('/api/watch', methods=['GET'])
 def get_watch():
-    response = requests.get(SERVICES_URL + '/watch', params=request.args)
+    response = requests.get(app.config['SERVICES_URL'] + '/watch', params=request.args)
     return response.text, response.status_code
 
 @app.route('/api/watch', methods=['POST'])
 def post_watch():
     token = get_login()
-    response = requests.post(SERVICES_URL + '/watch', auth=(token, ''), data=request.data)
+    response = requests.post(app.config['SERVICES_URL'] + '/watch', auth=(token, ''), data=request.data)
     return response.text, response.status_code
 
 @app.route('/api/watch/user', methods=['GET'])
 def get_watch_user():
     token = get_login()
-    response = requests.get(SERVICES_URL + '/watch/user/', auth=(token, ''), params=request.args)
+    response = requests.get(app.config['SERVICES_URL'] + '/watch/user/', auth=(token, ''), params=request.args)
     return response.text, response.status_code
 
 @app.route('/api/watch/<int:id>', methods=['GET'])
 def get_watch_by_id(id):
-    response = requests.get(SERVICES_URL + '/watch/%s' % id)
+    response = requests.get(app.config['SERVICES_URL'] + '/watch/%s' % id)
     return response.text, response.status_code
 
 @app.route('/api/watch/<int:id>', methods=['PUT'])
 def put_watch(id):
     token = get_login()
-    response = requests.put(SERVICES_URL + '/watch/%s' % id, auth=(token, ''), data=request.data)
+    response = requests.put(app.config['SERVICES_URL'] + '/watch/%s' % id, auth=(token, ''), data=request.data)
     return response.text, response.status_code
 
 @app.route('/api/watch/open', methods=['GET'])
 def get_watch_open():
     token = get_login()
-    resp = requests.get(SERVICES_URL + '/watch/open', auth=(token,''), params=request.args)
+    resp = requests.get(app.config['SERVICES_URL'] + '/watch/open', auth=(token,''), params=request.args)
     return resp.text, resp.status_code
 
 @app.route('/api/operator_event', methods=['GET'])
 def get_operator_event():
-    response = requests.get(SERVICES_URL + '/operator_event', params=request.args)
+    response = requests.get(app.config['SERVICES_URL'] + '/operator_event', params=request.args)
     return response.text, response.status_code
 
 @app.route('/api/operator_event', methods=['POST'])
 def post_event():
     token = get_login()
     print request.data
-    response = requests.post(SERVICES_URL + '/operator_event', auth=(token, ''), data=request.data)
+    response = requests.post(app.config['SERVICES_URL'] + '/operator_event', auth=(token, ''), data=request.data)
     return response.text, response.status_code
 
 @app.route('/api/operator_event_type', methods=['GET'])
 def get_operator_event_type():
-    response = requests.get(SERVICES_URL + '/operator_event_type', params=request.args)
+    response = requests.get(app.config['SERVICES_URL'] + '/operator_event_type', params=request.args)
     return response.text, response.status_code
