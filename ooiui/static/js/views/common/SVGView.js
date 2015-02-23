b779fd25158ee4c3437c0981b3d6b05f2723671d
birdage <abird@asascience.com>
2015-02-23 10:14:39 -0600

    add profile plotting to ui

--- ooiui/static/js/views/common/SVGView.js
+++ ooiui/static/js/views/common/SVGView.js
@@ -42,18 +42,29 @@ var SVGPlotView = SVGView.extend({
       }
     }
     if(this.variable != null) {
-      this.url = '/svg/plot/' + this.reference_designator + '/' + this.stream_name + '?' + $.param({yvar: this.variable, height: this.height, width: this.width })
+      //done on first render, i.e inital conditions      
+      var useLine = "True"
+      var useScatter = "False"
+      var plotLayoutType = "timeseries"
+      this.url = '/svg/plot/' + this.reference_designator + '/' + this.stream_name + '?' + $.param({yvar: this.variable, height: this.height, width: this.width, scatter:useScatter,lines:useLine, plotLayout:plotLayoutType })
+      console.log("reg url",this.url)
       this.fetch();
     }
   },
   plot: function(options) {
+    //requested plot
     this.reference_designator = this.model.get('reference_designator')
     this.stream_name = this.model.get('stream_name')
-    if(options && options.yvar) {
-      this.variable = options.yvar;
+    if(options && options.yvar && options.xvar) {
+      this.yvariable = options.yvar;
+      this.xvariable = options.xvar;
     }
-    if(this.variable != null) {
-      this.url = '/svg/plot/' + this.reference_designator + '/' + this.stream_name + '?' + $.param({yvar: this.variable, height: this.height, width: this.width })
+    if(this.yvariable != null && this.xvariable != null) {
+      this.useLine = options.useLine.toString();
+      this.useScatter = options.useScatter.toString();      
+      this.plotType = options.plotType;
+      
+      this.url = '/svg/plot/' + this.reference_designator + '/' + this.stream_name + '?' + $.param({yvar: this.yvariable , xvar: this.xvariable, height: this.height, width: this.width,scatter:this.useScatter,lines:this.useLine, plotLayout:this.plotType })
       this.fetch();
     }
   },
@@ -84,9 +95,11 @@ var SVGPlotView = SVGView.extend({
 
 var SVGPlotControlView = Backbone.View.extend({
   events: {
-    'change #yvar-select' : 'onClickPlot'
+    'change #xvar-select' : 'onClickPlot',
+    'change #yvar-select' : 'onClickPlot',
+    "switchChange.bootstrapSwitch .bootstrap-switch" : 'onClickPlot'
   },
-  initialize: function() {
+  initialize: function() { 
   },
   setModel: function(model) {
     var self = this;
@@ -102,17 +115,32 @@ var SVGPlotControlView = Backbone.View.extend({
     this.render();
   },
   template: JST['ooiui/static/js/partials/SVGPlotControl.html'],
-  onClickPlot: function(e) {
+  onClickPlot: function(e) {    
     var data = {};
+    
     data.start_date = this.$start_date_picker.getDate();
     data.end_date = this.$end_date_picker.getDate();
     data.xvar = this.$el.find('#xvar-select').val();
     data.yvar = this.$el.find('#yvar-select').val();
+    
+    if (data.xvar== "pressure"){
+      data.plotType = "depthprofile"
+      data.yvar = "pressure"
+      data.xvar = this.$el.find('#yvar-select').val();
+
+    }else{
+      data.plotType = "timeseries"
+    }
+
+    data.useLine = this.$el.find('#plotting-enable-line').bootstrapSwitch('state');
+    data.useScatter = this.$el.find('#plotting-enable-scatter').bootstrapSwitch('state');
+
     ooi.trigger('SVGPlotControlView:onClickPlot', data);
   },
   render: function() {
     this.$el.html(this.template({model: this.model}));
     this.$el.find('.selectpicker').selectpicker();
+    this.$el.find('.bootstrap-switch').bootstrapSwitch();
 
     var xvar = this.model.get('preferred_timestamp');
     
@@ -126,7 +154,7 @@ var SVGPlotControlView = Backbone.View.extend({
     this.$start_date_picker.setDate( this.model.get('start'));
     this.$end_date_picker.setDate( this.model.get('end'));
     
-    this.$el.find('#xvar-select').prop('disabled', 'disabled');
+    //this.$el.find('#xvar-select').prop('disabled', 'disabled');
     this.$el.find('#xvar-select').selectpicker('val', xvar);
   }
 });
