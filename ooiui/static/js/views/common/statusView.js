ea4176e29cc61c289330c87aa477428727c70d8b
Brian Smith <bsmith@asacience.com>
2015-02-26 10:14:53 -0500

    rough filter page

--- ooiui/static/js/views/common/statusView.js
+++ ooiui/static/js/views/common/statusView.js
@@ -1,9 +1,9 @@
 var StatusModel = Backbone.Model.extend({
 
   defaults:{
-    'display_name': "brian",
-    'reference_designator': "smith",
-    'type': 'Glider'
+    'display_name': "name",
+    'reference_designator': "123",
+    'type': 'none'
   }
 })
 
@@ -19,10 +19,9 @@ var StatusViews= Backbone.View.extend({
 
 
   initialize: function(){
-    _.bindAll(this, "render","getTypes","createSelect","setFilter","filterByType");
+    _.bindAll(this, "render","filterByType");
     
     this.on("change:filterType", this.filterByType, this);
-    // need to fix createSelect
     this.collection.on("add",this.render, this)
     this.collection.on("add", this.createSelect, this)
     this.collection.on("reset", this.render, this)
@@ -30,8 +29,7 @@ var StatusViews= Backbone.View.extend({
 
 
   render :function(){
-
-    
+        
     this.$el.html(this.template())
 
 
@@ -49,65 +47,70 @@ var StatusViews= Backbone.View.extend({
       
       var statusView = new StatusView({model: model});
       $('#status-view').append(statusView.el);
+ 
+   
     })
     
   },
-  getCollection: function(collection){
-    
+ 
 
-  },
-  getTypes:function(){
-    console.log(" get types called")
-    console.log(this.collection.pluck("type"))
-    var unique = _.uniq(this.collection.pluck("type"))
-    console.log(unique) 
-    return unique
-  },
-  createSelect:function(){
-    console.log(' create selectcalled')
-    var filter = this.$el.find("#filter"),
+// functions below are used to create a dynamic selection options. 
+// It will be used in conjunction with Uframe assests and events to create dynamic options
+
+
+  //  getTypes:function(){
+ //   console.log(" get types called")
+//    console.log(this.collection.pluck("type"))
+//    var unique = _.uniq(this.collection.pluck("type"))
+//    console.log(unique) 
+//    return unique
+//  },
+ // createSelect:function(){
+ //   console.log(' create selectcalled')
+ //   var filter = this.$el.find("#filter"),
     
-    select = $("<select/>", {
-      html: "<option value='all'>All</option>"
-    });
-    var get = this.getTypes()
-    console.log(get)
-     _(get).each(function(item){
-       console.log(item)
-       var option = $("<option/>", {
-           value: item.toLowerCase(),
-           text: item.toLowerCase()
-       }).appendTo(select);
-     });
-     console.log(select)
+ //   select = $("<select/>", {
+ //     html: "<option value='all'>All</option>"
+ //   });
+ //   var get = this.getTypes()
+ //   console.log(get)
+ //    _(get).each(function(item){
+ //      console.log(item)
+  //     var option = $("<option/>", {
+ //          value: item.toLowerCase(),
+ //          text: item.toLowerCase()
+ //      }).appendTo(select);
+//     });
+//     console.log(select)
      //this.$el.find("#filter").html(select);
      
-     filter.html(select);
-     return select;
-  },
+ //    filter.html(select);
+ //    return select;
+ // },
 
   events: {
     "change #filter select": "setFilter"
   },
 
-  setFilter: function(event){
-    console.log('set filter is called')
-    console.log(event.currentTarget.value);
-    this.filterType = event.currentTarget.value;
-    this.trigger("change:filterType")
-  },
-  filterByType:function(){
-    console.log(this.collection)
+ // setFilter: function(event){
+    //console.log('set filter is called')
+    //console.log(event.currentTarget.value);
+//    this.filterType = event.currentTarget.value;
+//    this.trigger("change:filterType")
+//  },
+  filterByType:function(type){
+    console.log(type)
+    //console.log(this.collection)
     console.log('filter by type is called')
     if (this.filterType === "all"){
       this.collection.reset(this.collection);
     } 
     else{
-      console.log(this.collection)
+     // console.log(this.collection)
       this.collection.reset(this.collection.models, { silent: true });
-      console.log(this.collection.models)
-      var filterType = this.filterType,
-      filtered = _.filter(this.collection.models,function (item){
+     // console.log(this.collection.models)
+      var filterType = type,
+        filtered = _.filter(this.collection.models,function (item){
         console.log(item.get("type"))
         return item.get("type").toLowerCase() === filterType;
                   });
@@ -117,7 +120,7 @@ var StatusViews= Backbone.View.extend({
           }
     
    // need to fix this 
-      this.$el.find("#filter").html(this.createSelect());
+     // this.$el.find("#filter").html(this.createSelect());
      }
 
       
@@ -129,6 +132,7 @@ var StatusView= Backbone.View.extend({
 
   initialize: function(){
     _.bindAll(this, "render");
+    this.listenTo(ooi.views.statusFilterView, 'change:resetButton', this.reset)
     this.render()
     //console.log(this.collection)
     //this.listenTo(this.collection, 'add', this.addstatus)
@@ -146,27 +150,53 @@ var StatusView= Backbone.View.extend({
   },
 
   render: function(){
-    //this.$el.html(this.template({model: this.model}))
+   // console.log(this.collection)
+   //console.log(this.model)
+   this.$el.html(this.template({model: this.model}))
   
-    this.$el.html(this.template({model: this.model}))
+    //this.$el.html(this.template({collection: this.collection}))
   },
 
   addstatus:function(){
     console.log('I LISTENED')
     
+  },
+  reset:function(){
+    console.log('statusview reset called')
+
+    $('#assetButton').remove()
   }
 
 })
 
 var StatusFilterView = Backbone.View.extend({
+  collection: StatusCollection,
   initialize: function(){
-    _.bindAll(this, "render");
+    _.bindAll(this, "render", "setFilter", "reset");
     this.render()
+        
+  },
+  events:{
+    "click #submitButton": "setFilter",
+    "click #resetButton": "reset"
+
   },
   template: JST['ooiui/static/js/partials/StatusFilter.html'],
 
   render:function(){
     this.$el.html(this.template())
+  },
+  setFilter: function(){
+    console.log('submit button render page')
+    var assetType =$("#assetType").val()
+    console.log(assetType)
+    console.log(this.collection)
+    ooi.views.statusViews.filterByType(assetType)
+  },
+  reset: function(){
+    console.log('reset called')
+    $('#assetButton').remove()
+    this.trigger('change:resetButton')
   }
 
 
