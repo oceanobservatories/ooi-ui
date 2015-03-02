1b590eee3c7dba5437c0e5f5ad2ef312f8648de6
Dan Langston <dan@danlangston.com>
2015-03-02 16:52:52 -0500

    endurance-array_fixed nameing conventions for locationSampling

--- ooiui/static/js/views/common/PioneerLocationSamplingView.js
+++ ooiui/static/js/views/common/PioneerLocationSamplingView.js
@@ -1,14 +1,14 @@
 "use strict";
 
 
-var LocationSamplingView = Backbone.View.extend({
+var PioneerLocationSamplingView = Backbone.View.extend({
 
   initialize: function() {
     _.bindAll(this, "render");
     var self = this;
     self.render();
   },
-  template: JST['ooiui/static/js/partials/LocationSampling.html'],
+  template: JST['ooiui/static/js/partials/PioneerLocationSampling.html'],
   render: function() {
     this.$el.html(this.template());
   } 
