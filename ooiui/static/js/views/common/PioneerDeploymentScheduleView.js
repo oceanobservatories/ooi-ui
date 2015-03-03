2e197e5c67757a695bd99f64d867d195f45fbd58
Dan Langston <dan@danlangston.com>
2015-03-03 12:52:28 -0500

    endruacne-array_added Pioneer to begining of all related files to keep code flow clear

--- ooiui/static/js/views/common/PioneerDeploymentScheduleView.js
+++ ooiui/static/js/views/common/PioneerDeploymentScheduleView.js
@@ -1,14 +1,14 @@
 "use strict";
 
 
-var StationSummaryArrayView = Backbone.View.extend({
+var PioneerDeploymentScheduleView = Backbone.View.extend({
 
   initialize: function() {
     _.bindAll(this, "render");
     var self = this;
     self.render();
   },
-  template: JST['ooiui/static/js/partials/StationSummaryArray.html'],
+  template: JST['ooiui/static/js/partials/PioneerDeploymentSchedule.html'],
   render: function() {
     this.$el.html(this.template());
   } 
