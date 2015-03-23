76ab620fab63b2bff5c5a921615dd8e6a6ea6184
James Case <case@oceanz.org>
2015-03-23 10:21:15 -0400

    Adds asset nav event.

--- ooiui/static/js/views/c2/ArrayNavView.js
+++ ooiui/static/js/views/c2/ArrayNavView.js
@@ -1,6 +1,10 @@
 var ArrayNavView = Backbone.View.extend({
+  // The el will be a <ul> tag
+  tagName: 'ul',
+  // <ul class="nav sidebar-nav navbar-collapse">
+  className: 'nav sidebar-nav navbar-collapse',
   events: {
-    'click button' : 'onClick'
+    'click a' : 'onClick'
   },
 
   onClick: function(event) {
@@ -10,7 +14,7 @@ var ArrayNavView = Backbone.View.extend({
     var array_id = $(event.target).attr('data-reference-designator');
     console.log('Array onClick: ' + array_id);
     // Publish a new method org:click with the model_id
-    //this.render();
+    this.render();
     this.selectArray(array_id);
   },
 
@@ -21,6 +25,11 @@ var ArrayNavView = Backbone.View.extend({
       //console.log(this.collection);
       this.render();
     });
+    var refdes = ooi.models.arrayNavModel.get('array_code');
+    if(refdes && refdes != '' && refdes != null) {
+      console.log('refdes: ' + refdes);
+      this.selectArray(refdes);
+    }
   }
   ,
   selectArray: function(refdes) {
@@ -32,7 +41,8 @@ var ArrayNavView = Backbone.View.extend({
     _(this.collection.models).each(function(model){
       //console.log(model);
       //$('#array_list').append('<div data-reference-designator='+ model.get('array_code')+' class="btn btn-default btn-sm" type="button" aria-label="...">'+ model.get('display_name')+'</div>');
-    $('#array_list').append('<button data-reference-designator='+ model.get('array_code')+' class="btn btn-default btn-sm">'+ model.get('display_name')+'</button>');
+      //$('#array_list').append('<button data-reference-designator='+ model.get('array_code')+' class="btn btn-default btn-sm">'+ model.get('display_name')+'</button>');
+      $('#array_list').append('<a data-reference-designator='+ model.get('array_code')+' class="btn btn-default btn-sm" href="#">'+ model.get('display_name')+'</a>');
     });
   }
 });
