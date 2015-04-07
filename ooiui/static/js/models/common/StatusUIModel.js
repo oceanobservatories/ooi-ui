3cdeaec2f0b2dbb8d4a2ec42a5e164abdc219e6a
Brian Smith <bsmith@asacience.com>
2015-04-07 12:54:14 -0400

    removes white space

--- ooiui/static/js/models/common/StatusUIModel.js
+++ ooiui/static/js/models/common/StatusUIModel.js
@@ -3,13 +3,10 @@ var StatusUIModel= Backbone.Model.extend({
    //urlRool: "/api/display_name?reference_designator=",
  // url: "http://localhost:4000/uframe/assets",
    get_display_name: function(){
-      
    var ref = this.get('ref_des');
    var xhr = new XMLHttpRequest();
    var self =this;
-
-
-  xhr.onreadystatechange = function() {
+   xhr.onreadystatechange = function() {
       if(xhr.readyState == 4 && xhr.status == 200) {
         //some assets are undefined, resulting in errors
         var myArr = JSON.parse(xhr.responseText);
@@ -20,13 +17,9 @@ var StatusUIModel= Backbone.Model.extend({
         //console.log("Error", xhr.statusText);
       }
    };
-
-  
    xhr.open('GET', 'http://localhost:5000/api/display_name?reference_designator='+ref);
    xhr.send();   
-
    },
-
   defaults: {
     assets: "",
     assetId: "",
@@ -58,11 +51,7 @@ var StatusUICollection= Backbone.Collection.extend({
  url: "/api/asset_deployment", 
  // url: "http://localhost:4000/uframe/assets",
  model: StatusUIModel,
-
  parse: function(response, options) {
-   
-       
    return response.assets;
   },
-  });  
-
+});  
--- ooiui/static/js/views/common/StatusUIIconView.js
+++ ooiui/static/js/views/common/StatusUIIconView.js
@@ -12,20 +12,15 @@ var StatusUIIconView = Backbone.View.extend({
     _.bindAll(this, "render", "onListView", "onTableView", 'filter'); //, "onSync"
     this.initialRender();
     var self = this;
-    
     this.listenTo(this.collection,'reset',function(){
       //not the best way to render
       //but I have to make a call to our db to get display names after fetch is called.
-      //
       setTimeout(function(){
          self.render();
       },5000);
     });
-      
-
     this.viewSelection = 'table';
   },
-
     // Spinner
   initialRender: function() {
     var self = this;
@@ -38,30 +33,15 @@ var StatusUIIconView = Backbone.View.extend({
                     $('#container_of_asset').append("<li id='"+response['assets'][t]['assetId']+"' data-Type='"+response['assets'][t].assetInfo['type']+"' data-Class='"+response['assets'][t]['class']+"' data-Owner='"+response['assets'][t].assetInfo['owner']+"'><span>Type: <i>"+response['assets'][t].assetInfo['type']+"</i></span><span>Class: <i>"+response['assets'][t]['class']+"</i></span><span>Owner: <i>"+response['assets'][t].assetInfo['owner']+"</span></li>");
                 
       }
-
            self.filter(response);
-
-
-
-
-
-        
-               
           _.map(collection.models, function(model){
                              return model.get_display_name();
            });
-               
-        
       }
-
-      
     });
-    
     this.$el.html('<i class="fa fa-spinner fa-spin" style="margin-left:50%;font-size:90px;"> </i>');
-    
   },
   filter: function(e){
-     
      var self= this;
      this.ft = $.filtrify('container_of_asset', 'asset_search_filter', {
       callback: function(query, match, mismatch){
@@ -72,8 +52,6 @@ var StatusUIIconView = Backbone.View.extend({
          _.each(match, function(modelId){
           var ID = parseInt(modelId.id);
           var filtered = self.collection.findWhere({assetId:ID});
-            
-         
         if(self.viewSelection === 'list'){
            var filterview = new StatusUIAccordionView({
                model: filtered
@@ -83,7 +61,6 @@ var StatusUIIconView = Backbone.View.extend({
 
            var filterview = new StatusUIItemView({
                model:filtered
-          
             });
             self.$el.find('#alert-container').append(filterview.el);
         }
@@ -91,15 +68,9 @@ var StatusUIIconView = Backbone.View.extend({
       }
      });
   },
-    
-
-    
-    
-
   add: function(subview) {
     this.$el.find('#alert-container').append(subview.el);
   },
-
   onListView: function() {
     this.viewSelection = 'list';
     this.ft.reset();
@@ -107,14 +78,11 @@ var StatusUIIconView = Backbone.View.extend({
     this.$el.find('#table-view').removeClass('active');
     this.$el.find('.Hide').show();
   },
-
   onTableView: function() {
     this.viewSelection = 'table';
     this.ft.reset();
     this.$el.find('.Hide').hide();
-
   },
-
   template: JST['ooiui/static/js/partials/StatusUIIcon.html'],
   render: function() {
     //render is called only once
@@ -122,7 +90,6 @@ var StatusUIIconView = Backbone.View.extend({
     var self = this;
     this.$el.html(this.template());
     this.$el.find('.Hide').hide();
-
     //set the table-view button as active on intial render
     this.$el.find('#table-view').toggleClass('active');
         this.collection.each(function(model) { 
@@ -133,5 +100,3 @@ var StatusUIIconView = Backbone.View.extend({
       });
   }
 });
-
-
