09e5898ad4bcfa2925887b0e0747fab0a292f667
M@Campbell <mcampbell@asascience.com>
2015-04-29 17:46:33 -0400

    adds route to support event return by reference designator

--- ooiui/core/routes/science.py
+++ ooiui/core/routes/science.py
@@ -151,6 +151,11 @@ def asset_event_post():
     response = requests.post(app.config['SERVICES_URL'] + '/uframe/events', data=request.data)
     return response.text, response.status_code
 
+@app.route('/api/events/<string:ref_des>', methods=['GET'])
+def get_event_by_ref_des(ref_des):
+    response = requests.get(app.config['SERVICES_URL'] + '/uframe/events/%s' % id, data=request.data)
+    return response.text, response.status_code
+
 
 @app.route('/opLog.html')
 def op_log():
