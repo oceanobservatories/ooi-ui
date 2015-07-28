2b6e761bc2a96b025919ecf2e9031df011b6d310
Daniel Langston <dlangston@dLangstons-MacBook-Pro.local>
2015-07-28 12:31:13 -0400

    changed moorings label back to site

--- ooiui/static/js/views/science/PlottingSelectionView.js
+++ ooiui/static/js/views/science/PlottingSelectionView.js
@@ -22,9 +22,9 @@ var PlottingSelectionView = Backbone.View.extend({
     itemid:null,
   },
   parentChild : {
-                 "arrays":{"parent":null,"child":"moorings"},
-                 "moorings":{"parent":"arrays","child":"platforms"},    
-                 "platforms":{"parent":"moorings","child":"instruments"},                                   
+                 "arrays":{"parent":null,"child":"sites"},
+                 "sites":{"parent":"arrays","child":"platforms"},    
+                 "platforms":{"parent":"sites","child":"instruments"},                                   
                  "instruments":{"parent":"platforms","child":"streams"},
                  "streams":{"parent":"streams","child":"instruments"},
                  "parameters":{"parent":"instruments","child":"parameters"}
@@ -240,8 +240,8 @@ var PlottingSelectionView = Backbone.View.extend({
 
         //if we selected a platform the subsequent items need to have their mooring checked, at platform is not unique
         if (currentItem == "platforms"){          
-          var platchildCode = $( this ).attr(currentItem+"-code") + $( this ).attr("moorings-code"); 
-          var plotfilterVal = $(selected).not("[disabled]").attr(currentItem+"-code") + $(selected).not("[disabled]").attr("moorings-code")
+          var platchildCode = $( this ).attr(currentItem+"-code") + $( this ).attr("sites-code"); 
+          var plotfilterVal = $(selected).not("[disabled]").attr(currentItem+"-code") + $(selected).not("[disabled]").attr("sites-code")
 
           if (plotfilterVal == platchildCode){                              
             $( this ).text(childValue)
