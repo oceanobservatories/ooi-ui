db5d4a4f55a86343d528ef2e21304f4527fdf321
James Case <case@oceanz.org>
2015-08-04 16:11:05 -0400

    Adds support for uWSGI/nginx deployment.

---
+++ WSGI.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''
+WSGI.py
+'''
+import argparse
+from flask import jsonify, url_for
+from ooiui.core.app import app
+import ooiui.core.routes.common
+import ooiui.core.routes.science
+import ooiui.core.routes.c2
+import ooiui.core.routes.aa
+
+if __name__ == '__main__':
+    app.run(host='localhost:5000', debug=True)
