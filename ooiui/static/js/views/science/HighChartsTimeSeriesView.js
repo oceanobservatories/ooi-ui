90149eb004580d4341d5f2424de73aa8c467b0c5
birdage <abird@asascience.com>
2015-08-05 13:43:03 -0400

    add high charts type plot to plot page

---
+++ ooiui/static/js/views/science/HighChartsTimeSeriesView.js
@@ -0,0 +1,88 @@
+/*
+ * ooiui/static/js/views/science/HighChartsTimeSeriesView.js
+ * View definition for the time series container
+ *
+ * Partials:
+ *  - ooiui/static/js/partials/Timeseries.html
+ */
+var TimeseriesView = Backbone.View.extend({
+  className: 'timeseries-view',
+  events: {    
+  },  
+  views: {
+    highchartsView: null
+  },
+  initialize: function(options) {
+    //_.bindAll(this, 'onBack', 'onExplore');    
+    //this.colorPalette = options.colorPalette;
+    this.initialRender();
+    this.views.highchartsView = new HighchartsView({
+      title: "Title",
+      title_style: {
+        fontSize: "20px",
+        color: "steelblue"
+      }
+    });
+  },
+
+  initialRender: function() {
+    this.$el.html('<i class="fa fa-spinner fa-spin" style="margin-top:40px;margin-left:40%;font-size:90px;"> </i>');
+  },
+  template: JST['ooiui/static/js/partials/Timeseries.html'],
+  modifytime:function(time_sec){
+    time_sec -= 2208988800
+    var d = moment.utc(time_sec);        
+    return d._i*1000;   
+  },
+  render: function() {
+    var self = this;
+    this.$el.html(this.template());
+
+    //get the title from the collection
+    this.views.highchartsView.title = self.collection.getTitle();
+    this.views.highchartsView.subtitle = self.collection.getSubtitle();
+    
+    /* Build a list of series */
+    var seriesCollection = new SeriesCollection();
+
+    _.each(self.collection.xparameters, function(param,index) { 
+
+      var xvar = self.collection.xparameters[index]
+      var yvar = self.collection.yparameters[index]
+      var series_data = [];
+
+      //loop over a collection of params make uframe return look like highcharts data
+      self.collection.each(function(model) {        
+        //get the data, and convert if its date time
+        if (xvar == "time"){
+          series_data.push([self.modifytime(model.get(xvar)),model.get(yvar)]);
+        }else if (yvar == "time"){
+          series_data.push([model.get(xvar),self.modifytime(model.get(yvar))]);
+        }else{
+          series_data.push([model.get(xvar),model.get(yvar)]);        
+        }      
+      });
+
+      var seriesModel = new SeriesModel({data:series_data})
+      seriesModel.set('units', self.collection.getUnits(yvar));
+      seriesModel.set('name', yvar);
+      seriesModel.set('axisName', yvar+" ("+self.collection.getUnits(yvar)+")");
+      seriesCollection.add(seriesModel);
+
+    });
+
+    /*
+    var colorPalette = this.colorPalette.clone()
+    
+    seriesCollection.each(function(seriesModel) {
+      seriesModel.set('color',colorPalette.chooseColor());
+    });
+    */
+
+    this.views.highchartsView.collection = seriesCollection;
+    this.views.highchartsView.setElement(this.$el.find('#highcharts-view'));
+    this.views.highchartsView.render();
+    return this;
+  }
+});
+
---
+++ ooiui/static/js/views/science/HighchartsView.js
@@ -0,0 +1,164 @@
+"use strict";
+/*
+ * ooiui/static/js/views/science/HighchartsView.js
+ */
+var HighchartsView = Backbone.View.extend({
+  initialize: function(options) {
+    this.title = options && options.title || "Chart";
+    this.title_style = options && options.title_style || {
+    };
+    this.subtitle = options && options.subtitle || "";
+    this.initialRender();
+    _.bindAll(this, "onClick");
+  },
+  initialRender: function() {
+    this.$el.html('<i class="fa fa-spinner fa-spin" style="margin-top:80px;margin-left:50%;font-size:90px;"> </i>');
+    return this;
+  },
+  onClick: function(e, point) {
+    this.trigger('onClick', e, point);
+  },
+  render: function() {
+    var self = this;
+    /* We use the d3 formatter because it's easy */
+    var formatter = d3.format(".2f");
+    var uniqAxis = [];
+    /* Generate uniqe axis for the series */
+    this.collection.each(function(model, i) {
+      var axis = _.find(uniqAxis, function(item) {
+        return item.title.text == model.get('axisName');
+      });
+
+      if(i % 2 === 0) {
+        var opposite_val = true;
+      }else{        
+        var opposite_val = false;
+      }
+      
+
+      if(_.isUndefined(axis)) {
+        axis = {          
+          labels: {
+            format: '{value}',
+            style: {
+              color: Highcharts.getOptions().colors[i]
+            }
+          },
+          title: {
+            text: model.get('axisName'),
+            style: {
+              color: Highcharts.getOptions().colors[i]
+            }
+          },
+          opposite:opposite_val
+        };
+        if (model.get('axisRange') && model.get('tickInterval')) {
+          _.extend(axis,{
+            min           : model.get('axisRange')[0],
+            max           : model.get('axisRange')[1],
+            tickInterval  : model.get('tickInterval')
+          });
+        }
+        if (i > 0) {
+          _.extend(axis,{
+            gridLineWidth : 0 // don't show anything other than the 1st axis' grid lines
+          });
+        }
+        uniqAxis.push(axis);
+      }
+    });
+
+
+
+
+    /* Actually produce the chart */
+    this.chart = new Highcharts.Chart({
+      chart: {
+        renderTo: this.el,
+        events: {
+          click: function(e) {
+            self.onClick(e, this);
+          },
+        },
+        alignTicks: false,      
+        zoomType: 'xy'      
+      },
+      credits: {
+        enabled: false
+      },
+      title: {
+          text: self.title,
+          style: self.title_style
+      },
+      subtitle: {
+          text: self.subtitle
+      },
+      xAxis: {
+          type: 'datetime',
+          dateTimeLabelFormats: { // don't display the dummy year
+            day: '%e %b %Y',
+            week: '%e %b %Y',
+            month: '%e %b %Y',
+            year: '%Y'
+          },
+          title: {
+              text: 'Date (UTC)'
+          }
+      },
+      yAxis: uniqAxis,
+      tooltip: {
+        useHTML: true,
+              formatter: function() {
+                  var x = this.x;
+                  var s = '';
+                  s+='<p><b>Time: '+Highcharts.dateFormat('%Y-%m-%d %H:%M:%S UTC',  this.x) +'</b></p><p>';
+
+                  _.each(self.chart.series,function(series) {
+                    if (series.visible) {
+                      // Assume the data is ordered by time.  Find the 1st value that is >= x.
+                      var xy = _.find(series.data,function(o){return o.x >= x});
+                      if (xy) {
+                        s += '<span style="color: ' + series.color + ';">' + series.name + '</span>' + ': ' + formatter(xy.y) + ' ' + series.options.units + '</p>';
+                      }
+                    }
+                  });
+                  return s;
+              },
+          shared: true,
+          crosshairs : [true,false]        
+      },
+      plotOptions: {        
+        scatter: {
+            marker: {
+                radius: 5,
+                states: {
+                    hover: {
+                        enabled: true,
+                        lineColor: 'rgb(100,100,100)'
+                    }
+                }
+            }            
+        }
+      },
+      series: self.collection.map(function(model, i) {
+        return {
+          type: model.get('type'),
+          name: model.get('name'),
+          showInLegend: model.get('showInLegend'),
+          color: model.get('color'),
+          data: model.get('data'),
+          units: model.get('units'),    
+          yAxis: i,                             
+          marker: {enabled: _.isUndefined(model.get('enableMarker')) ? false : model.get('enableMarker')}, // only dots are for direction
+          states: {hover: {enabled: false}} // no highlighted dots
+        };
+      }),
+      exporting: { //Enable exporting images
+        enabled: true,
+        enableImages: true
+      }
+    });
+    this.trigger('rendered', this);
+    return this;
+  }
+});
