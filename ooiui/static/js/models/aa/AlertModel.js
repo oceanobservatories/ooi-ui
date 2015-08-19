1c86dfde1fc7e139638d3de039b6c014a98dfce5
birdage <abird@asascience.com>
2015-08-19 14:49:59 -0400

    adds in the ability to create an A/A

--- ooiui/static/js/models/aa/AlertModel.js
+++ ooiui/static/js/models/aa/AlertModel.js
@@ -15,22 +15,61 @@
  * 
  */
 
+
+//used for the metadata about a param
+var AlertMetadataModel = Backbone.Model.extend({
+    url:"#", 
+    defaults:{
+        fillValue: "", //number in str format
+        particleKey: "",  //param name
+        pdId: "",    //id for the param
+        shape: "", //shape type
+        stream: "", //stream name        
+        type: "",   //param type
+        units: "",  //param units
+        unsigned: false   //not sure....
+    }    
+});    
+
+//holds the return of the collection
+var AlertMetadataCollection = Backbone.Collection.extend({            
+    model:AlertMetadataModel,
+    stream_model: null,
+    url: function(){        
+        return "/alerts/get_instrument_metadata/"+this.stream_model.get('reference_designator')+"/"+this.stream_model.get('stream_name');
+    },      
+    parse: function(response, options) {            
+        return response.stream_metadata      
+    }
+});    
+
+
+//alert model
 var AlertModel = Backbone.Model.extend({
-    defaults: {
-        //uframe_event_id: "default",
-        //dreference_designator: "default"
+    defaults: {        
     },
-    url:"/api/aa/alerts",
-    
+    url:"/api/aa/alerts",    
     validation: {
-        reference_designator: {
+        uframe_filter_id:{
+            required: false
+        },
+        uframe_event_id:{
+            required: false
+        },
+        active: {
             required: false
         },
         array_name: {
             required: false
         },
-        platform_name:{
+        description:{
             required: false
+        },
+        event_type:{
+            required: false  
+        },
+        high_value:{
+            required: false  
         },
         instrument_name: {
             required: false
@@ -38,25 +77,58 @@ var AlertModel = Backbone.Model.extend({
         instrument_parameter: {
             required: false
         },
+        instrument_parameter_pdid: {
+            required: false
+        },
+        low_value:{
+            required: false  
+        },
         operator:{
             required: false
         },
-        values: {
+        platform_name:{
             required: false
         },
-        created_time: {
+        reference_designator: {
             required: false
-        },
-        priority:{
-            required:true 
-        },
-        active: {
+        },             
+        severity:{
             required: false
-        },
-        description:{
+        },                
+        stream: {
             required: false
+        },        
+        escalate_on:{
+            required:false 
+        },
+        escalate_boundary:{
+            required:false 
+        },
+        use_email:{
+            required:false 
+        },
+        use_log:{
+            required:false 
+        },
+        use_phone:{
+            required:false 
+        },
+        use_redmine:{
+            required:false 
+        },
+        use_sms:{
+            required:false 
+        },
+        user_id: {
+            required:false 
         }
     }
+});
+
+//full collection of alerts
+var AlertsFullCollection = Backbone.Collection.extend({
+    model: AlertModel,
+    url:"/api/aa/alerts"
 });
 
 var TriggeredAlertModel = Backbone.Model.extend({
@@ -92,7 +164,7 @@ var TriggeredAlertModel = Backbone.Model.extend({
             required: false
         },
         priority:{
-            required:true 
+            required: true 
         },
         active: {
             required: false
@@ -101,13 +173,6 @@ var TriggeredAlertModel = Backbone.Model.extend({
             required: false
         }
     }
-});
-
-
-//full collection of alerts
-var AlertsFullCollection = Backbone.Collection.extend({
-    model: AlertModel,
-    url:"/api/aa/alerts"
 });
 
 
