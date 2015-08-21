4b12a7179c4604860962a114eb56618d5da27281
dlangton <dan.langston@rpsgroup.com>
2015-08-21 11:49:13 -0400

    added full breadcrumb to modal

--- ooiui/static/js/views/science/StreamDownloadFormView.js
+++ ooiui/static/js/views/science/StreamDownloadFormView.js
@@ -84,8 +84,8 @@ var StreamDownloadFormView = Backbone.View.extend({
     this.$start_date_picker.setDate(startDate);
     this.$end_date_picker.setDate(endDate);
     
-    if(model.get('display_name') != null) {
-      this.$el.find('#streamName').text(model.get('display_name'));
+    if(model.get('long_display_name') != null) {
+      this.$el.find('#streamName').text(model.get('long_display_name'));
     }else{
       this.$el.find('#streamName').text(model.get('stream_name'));
     }
