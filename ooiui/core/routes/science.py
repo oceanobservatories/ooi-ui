6002f3d91babd87e7addfc4c07f5ac4a76667394
M@Campbell <mcampbell@asascience.com>
2015-12-14 11:00:58 -0500

    removes the plotting page, and disables the download of non-large file types

--- ooiui/core/routes/science.py
+++ ooiui/core/routes/science.py
@@ -59,6 +59,7 @@ def streams_page():
     return render_template('science/streams.html')
 
 
+
 @app.route('/antelope_acoustic/')
 @login_required()
 def acoustics_page():
@@ -78,7 +79,8 @@ def show_plotting_no_path():
 @login_required()
 def plotting_page(path):
     urllib2.urlopen(app.config['GOOGLE_ANALYTICS_URL'] + '&dp=%2Fplotting')
-    return render_template('science/plotting.html')
+    # return render_template('science/plotting.html')
+    return render_template('common/tempPlotting.html')
 
 
 @app.route('/getdata/')
