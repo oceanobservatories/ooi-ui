0f6ad92682d872ee94ebeab5c9b3798ac3257c78
M@Campbell <mcampbell@asascience.com>
2016-02-12 07:18:10 -0500

    allows OR searching on scientific concepts

--- ooiui/static/js/models/common/PageControlModels.js
+++ ooiui/static/js/models/common/PageControlModels.js
@@ -25,43 +25,62 @@ var TocPageControlModel = Backbone.Model.extend({
 
         params : {
             "General":[
-                ["temperature", "Temperature"],
-                ["pressure", "Pressure"],
-                ['ph', 'pH'],
-                ['nitrate', 'Nitrate'],
-                ["co2", "CO2"],
-                ['density', 'Density'],
-                ['salinity', 'Salinity']
+                ["CTDAVN CTDBPC CTDBPD CTDBPE CTDBPF CTDBPN CTDBPO CTDBPP CTDGVM CTDMOG CTDMOH CTDMOQ CTDMOR CTDPFA CTDPFB CTDPFJ CTDPFK CTDPFL", "Water Temperature"],
+                ["CTDAVN CTDBPC CTDBPD CTDBPE CTDBPF CTDBPN CTDBPO CTDBPP CTDGVM CTDMOG CTDMOH CTDMOQ CTDMOR CTDPFA CTDPFB CTDPFJ CTDPFK CTDPFL", "Pressure"],
+                ['PHSENA PHSENB PHSEND PHSENE PHSENF', 'pH'],
+                ['NUTNRB NUTNRA NUTNRM', 'Nitrate'],
+                ['CTDAVN CTDBPC CTDBPD CTDBPE CTDBPF CTDBPN CTDBPO CTDBPP CTDGVM CTDMOG CTDMOH CTDMOQ CTDMOR CTDPFA CTDPFB CTDPFJ CTDPFK CTDPFL', 'Density'],
+                ['CTDAVN CTDBPC CTDBPD CTDBPE CTDBPF CTDBPN CTDBPO CTDBPP CTDGVM CTDMOG CTDMOH CTDMOQ CTDMOR CTDPFA CTDPFB CTDPFJ CTDPFK CTDPFL', 'Salinity']
             ],
             "Air Sea Interface":[
-                ['humidity', 'Humidity'],
-                ['wind_velocity', 'Wind Velocity'],
-                ['precipitation', 'Precipitation'],
-                ['air_sea_co2_flux', 'Air Sea CO2 Flux'],
-                ['air_sea_heat_flux', 'Air Sea Heat Flux'],
-                ['wave_properties', 'Wave Properties']
+                ['FDCHPA METBKA', 'Air Temperature'],
+                ['FDCHPA METBKA', 'Humidity'],
+                ['FDCHPA METBKA', 'Wind Velocity'],
+                ['METBKA', 'Precipitation'],
+                ['PC02AA', 'Atmospheric CO2'],
+                ['PC02AA', 'Air-Sea CO2 Flux'],
+                ['PC02WA PC02WB PC02WC PC02AA', 'Seawater CO2'],
+                ['METBKA FDCHPA', 'Air-Sea Heat Flux'],
+                ['METBKA', 'Downwelling Irradiance'],
+                ['METBKA', 'Atmospheric Pressure'],
+                ['CTDAVN CTDBPC CTDBPD CTDBPE CTDBPF CTDBPN CTDBPO CTDBPP CTDGVM CTDMOG CTDMOH CTDMOQ CTDMOR CTDPFA CTDPFB CTDPFJ CTDPFK CTDPFL METBKA', 'Water Temperature'],
+                ['CTDAVN CTDBPC CTDBPD CTDBPE CTDBPF CTDBPN CTDBPO CTDBPP CTDGVM CTDMOG CTDMOH CTDMOQ CTDMOR CTDPFA CTDPFB CTDPFJ CTDPFK CTDPFL METBKA', 'Salinity'],
+                ['METBKA', 'Humidity'],
+                ['WAVSSA', 'Wave Properties']
             ],
             "Water Column": [
-                ['downwelling_irradiance', 'Downwelling Irradiance'],
-                ['par', 'PAR'],
-                ['chlorophyll', 'Chlorophyll'],
-                ['dissolved_o2', 'Dissolved O2'],
-                ['water_velocity', 'Water Velocity'],
+                ['SPKIRA SPKIRB SPKIRJ', 'Downwelling Irradiance'],
+                ['PARADA PARADJ PARADK PARADM', 'PAR'],
+                ['FLNTUA FLCDRA FLORDD FLORDL FLORDM FLORTD FLORTJ FLORTK FLORTM', 'Chlorophyll'],
+                ["DOFSTK DOSTAD DOSTAJ DOSTAL DOSTAM", "Dissolved O2"],
+                ['VADCPA VEL3DB VEL3DC VEL3DD VEL3DK VEL3DL VELPTA VELPTB VELPTD VELPTJ ADCPAM ADCPSI ADCPSJ ADCPSK ADCPSL ADCPSN ADCPTA ADCPTB ADCPTD ADCPTE ADCPTF ADCPTG ADCPTM', 'Water Velocity'],
+                ['ADCPAM ADCPSI ADCPSJ ADCPSK ADCPSL ADCPSN ADCPTA ADCPTB ADCPTD ADCPTE ADCPTF ADCPTG ADCPTM', 'Acoustics'],
+                ['ADCPTA ADCPTB ADCPTD ADCPTE ADCPTF ADCPTG ADCPTM', 'Wave Properties'],
                 ['microbial_dna', 'Microbial DNA'],
                 ['depth', 'Depth'],
-                ['turbidity', 'Turbidity'],
-                ['seismic_activity', 'Seismic Activity'],
+                ['FLNTUA FLCDRA FLORDD FLORDL FLORDM FLORTD FLORTJ FLORTK FLORTM', 'Turbidity'],
             ],
             "Seafloor": [
-                ['seafloor_movement', 'Seafloor Movement'],
-                ['hydrothermal_vent_fluid_chemistry', 'Hydrothermal Vent Fluid Chemistry'],
-                ['bethnic_fluid_flow_rate', 'Benthic Fluid Flow Rate'],
+                ['HPIESA', 'Water Velocity'],
+                ['HPIESA', 'Acoustics'],
+                ['HYDBBA HYDBBA', 'Acoustics'],
+                ['MASSPA THSPHA TRHPHA TMPSFA', 'Hydrothermal Vent Chemistry'],
+                ['TMPSFA HPIESA', 'Water Temperature'],
+                ['OBSBBA OBSSPA', 'Seafloor Movement'],
+                ['OBSBBA OBSSPA', 'Seismic Activity'],
+                ['OPTAAC OPTAAD OPTAAJ', 'Chlorophyll'],
+                ['OPTAAC OPTAAD OPTAAJ', 'Turbidity'],
+                ['PRESFA PRESFB PRESFC PRESTA PRESTB HPIESA', 'Seafloor Pressure'],
+                ['benthic_fluid_flow_rate', 'Benthic Fluid Flow Rate'],
             ],
             "Large Format Data":[
                 ['antelope', 'Hydrophone'],
-                ['camds', 'Still Images'],
-                ['camhd', 'HD Video'],
-                ['zplsc', 'Bioacoustic Sonar']
+                ['CAMDSC CAMDSB', 'Still Images'],
+                ['CAMHDA', 'HD Video'],
+                ['ZPLSCB ZPLSCC', 'Acoustics'],
+                ['BOTPTA', 'Seafloor Movement'],
+                ['BOTPTA', 'Seafloor Pressure'],
+                ['BOTPTA', 'Seismic Activity']
             ]
         }
     }
