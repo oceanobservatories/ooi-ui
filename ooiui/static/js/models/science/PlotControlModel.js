66cd9aa358978c305e133f71881a1db3a3871c89
birdage <abird@asascience.com>
2016-05-16 14:48:11 -0400

    add default plot selection

--- ooiui/static/js/models/science/PlotControlModel.js
+++ ooiui/static/js/models/science/PlotControlModel.js
@@ -38,10 +38,18 @@ var PlotControlModel = Backbone.Model.extend({
         invertX: false,
         invertY: false,
         plotTypeOptions: new PlotTypeCollection([
-                          {name:'X-Y', value:"xy", num_inputs: 6, inputs:["x","y"] },
-                          {name:'Rose', value:"rose", num_inputs: 2, inputs: ["value", "direction"] },
-                          {name:'Feather',value:"feather", num_inputs: 2, inputs: ["value", "direction"] },
-                          {name:'Psuedocolor',value:"pcolor", num_inputs: 3, inputs: ["x","y","color"] }
+                          {name:'X-Y', value:"xy", num_inputs: 6, inputs:[{name:"x","value":"x"},
+                                                                          {name:"y","value":"y"}]},
+
+                          {name:'Rose', value:"rose", num_inputs: 2, inputs:[{name:"value","value":"x"},
+                                                                            {name:"direction","value":"y"}]},
+
+                          {name:'Feather',value:"feather", num_inputs: 2, inputs: [{name:"magnitude","value":"x"},
+                                                                                   {name:"direction","value":"y"}]},
+
+                          {name:'Psuedocolor',value:"pcolor", num_inputs: 3, inputs: [{name:"x","value":"x"},
+                                                                                      {name:"y","value":"y"},
+                                                                                      {name:"color","value":"z"}]},
                          ]),
 
         plotStyleOptions: [
