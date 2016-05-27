99cc77230c8792ead1bc9a8fbacf0373db891d95
Dan Maher <dmaher@asascience.com>
2016-05-27 10:10:02 -0400

    WIP

--- ooiui/core/routes/m2m.py
+++ ooiui/core/routes/m2m.py
@@ -8,23 +8,27 @@ from ooiui.core.app import app
 from flask import request, render_template, Response, jsonify
 from flask import stream_with_context
 from ooiui.core.routes.common import get_login
+from ooiui.core.routes.decorators import login_required, scope_required
 import requests
 import urllib2
 
 #M2M Interface
-
-@app.route('/api/m2m/get_data/<string:reference_designator>/<string:method>/<string:stream>/<string:start>/<string:end>')
-def get_data(reference_designator, method, stream, start, end):
+@login_required()
+@scope_required('data_manager')
+@app.route('/api/m2m/get_data/')
+def get_data():
     token = get_login()
-    url = app.config['SERVICES_URL'] + '/m2m/get_data/%s/%s/%s/%s/%s' % (reference_designator, method, stream, start, end)
-    print url
-    for each in request.args:
-        print each, request.args[each]
-    response = requests.get(url, auth=(token, ''), params=request.args)
-    print response.text
-    return response.text, response.status_code
+    print token
+    params = ''
+    for term in request.args:
+        if term != 'uframe':
+            params =  params +  '&' + term + '=' + request.args.get(term)
+    url = app.config['SERVICES_URL'] + '/m2m/get_data/?uframe=%s' %(request.args.get('uframe').replace('?','(')+params.replace('&',')'))
+    data = requests.get(url, auth=(token, ''), params=request.args)
+    return data.text
 
 
+#@login_required()
 @app.route('/api/m2m/get_metadata/<string:reference_designator>', methods=['GET'])
 def get_metadata(reference_designator):
     '''
