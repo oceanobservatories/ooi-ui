97890f802132f847bf2f0b8560ce0db1c2484d02
Dan Maher <dmaher@asascience.com>
2016-06-07 10:03:51 -0400

    Fixes Cabled Array not showing

--- ooiui/static/js/models/asset_management/PlatformModel.js
+++ ooiui/static/js/models/asset_management/PlatformModel.js
@@ -106,8 +106,8 @@ var PlatformCollection = Backbone.Collection.extend({
     },
     byArray: function(array) {
         var filtered = this.filter(function (platform) {
-            if (platform.get('reference_designator') !== "" && platform.get('reference_designator').length === 8 ||
-                platform.get('reference_designator').indexOf('GL') > -1) {
+            if (platform.get('reference_designator') !== "" && platform.get('reference_designator').length === 14 ||
+                platform.get('reference_designator').indexOf('GL') > -1 || platform.get('reference_designator').length === 8) {
                 return platform.get('reference_designator').substr(0,2) === array;
             }
         });
