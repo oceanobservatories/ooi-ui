f471add03a41f8b5393e6c5e9ec2b205222847ce
James Case <case@oceanz.org>
2016-08-24 13:11:05 -0400

    Adds m2m route to WSGI.py

--- WSGI.py
+++ WSGI.py
@@ -9,6 +9,7 @@ import ooiui.core.routes.common
 import ooiui.core.routes.science
 import ooiui.core.routes.c2
 import ooiui.core.routes.aa
+import ooiui.core.routes.m2m
 
 if __name__ == '__main__':
     app.run(host='localhost:5000', debug=True)
