14a43546f6ef0943e48588581e1d1dd8c2f6b8e4
Peter Cable <petercable@gmail.com>
2016-10-12 15:25:25 -0700

    Added stream_name to AnnotationTableView model

--- ooiui/static/js/views/science/AnnotationTableView.js
+++ ooiui/static/js/views/science/AnnotationTableView.js
@@ -35,6 +35,10 @@ var AnnotationTableView = Backbone.View.extend({
         label : 'Reference Designator'
       },
       {
+        name : 'stream_name',
+        label : 'Stream Name'
+      },
+      {
         name : 'beginDT',
         label : 'Start Date'
       },
