6598b0bdeb32bd7034d547b3974501ab8d476882
Peter Cable <petercable@gmail.com>
2016-12-06 12:49:54 -0500

    Only forward the date and content-type headers for M2M requests

--- ooiui/core/routes/m2m.py
+++ ooiui/core/routes/m2m.py
@@ -13,12 +13,15 @@ from ooiui.core.app import app
 @app.route('/api/m2m', methods=['GET'], defaults={'path': ''})
 @app.route('/api/m2m/<path:path>', methods=['GET'])
 def m2m_handler(path):
+    transfer_header_fields = ['Date', 'Content-Type']
     app.logger.info(path)
     if request.authorization:
         api_user_name = request.authorization['username']
         api_user_token = request.authorization['password']
         url = app.config['SERVICES_URL'] + '/m2m/%s' % path
         response = requests.get(url, auth=(api_user_name, api_user_token), params=request.args)
-        return response.text, response.status_code, dict(response.headers)
+        headers = dict(response.headers)
+        headers = {k: headers[k] for k in headers if k in transfer_header_fields}
+        return response.text, response.status_code, headers
     else:
         return 'Please supply your API credentials', 401
