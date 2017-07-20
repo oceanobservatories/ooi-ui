ae4caf61c9c51c92ebdcda0c488d91fe8307de36
James Case <case@oceanz.org>
2017-07-20 10:03:32 -0400

    Adds DELETE method for M2M routes.

--- ooiui/core/routes/m2m.py
+++ ooiui/core/routes/m2m.py
@@ -12,8 +12,8 @@ from flask import render_template
 from ooiui.core.app import app
 
 
-@app.route('/api/m2m', methods=['GET', 'PUT', 'POST'], defaults={'path': ''})
-@app.route('/api/m2m/<path:path>', methods=['GET', 'PUT', 'POST'])
+@app.route('/api/m2m', methods=['GET', 'PUT', 'POST', 'DELETE'], defaults={'path': ''})
+@app.route('/api/m2m/<path:path>', methods=['GET', 'PUT', 'POST', 'DELETE'])
 def m2m_handler(path):
     transfer_header_fields = ['Date', 'Content-Type']
     #app.logger.info(path)
@@ -40,8 +40,14 @@ def m2m_handler(path):
                                     params=request.args,
                                     stream=True,
                                     data=request.data)
+
+        elif request.method == 'DELETE':
+            response = requests.delete(url,
+                                    auth=(api_user_name, api_user_token),
+                                    params=request.args)
+
         else:
-            return 'Improper request method \'%s\', only GET, PUT and POST are supported. ' % request.method
+            return 'Improper request method \'%s\', only GET, PUT, POST and DELETE are supported. ' % request.method
         headers = dict(response.headers)
         headers = {k: headers[k] for k in headers if k in transfer_header_fields}
         return Response(response.iter_content(1024), response.status_code, headers)
