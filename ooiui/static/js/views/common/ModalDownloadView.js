31bc9a50e8ee27b171f041ea15a6f3435d3bea6e
James Case <case@oceanz.org>
2018-05-17 12:13:43 -0400

    Adds time and size estimate into download confirmation dialog.

--- ooiui/static/js/views/common/ModalDownloadView.js
+++ ooiui/static/js/views/common/ModalDownloadView.js
@@ -33,10 +33,33 @@ var ModalDownloadView= Backbone.View.extend({
     }
     return baseString + time + units + '.</p>';
   },
-  show: function(url, timeCalculation) {
+  bytesToSize: function(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return 'n/a';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    if (i === 0) return bytes + ' ' + sizes[i];
+    return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+  },
+  getSizeString: function(size) {
+    // time = number of seconds
+
+    // If time is null, don't worry about it
+    if (_.isNull(size)){
+      return '';
+    }
+
+    var baseString = "<p>Estimated file download size: ";
+    var sizes = this.bytesToSize(size);
+
+
+    return baseString + sizes + '.</p>';
+  },
+  show: function(url, timeCalculation, sizeCalculation) {
     var timeString = this.getTimeString(timeCalculation);
+    var sizeString = this.getSizeString(sizeCalculation);
     this.$el.find('#download-url').html('<p>A link containing your data will be emailed to <b>' + url +'</b></p>');
     this.$el.find('#download-url').append(timeString);
+    this.$el.find('#download-url').append(sizeString);
     this.$el.find('#download-ok').modal('show');
   },
   template: JST['ooiui/static/js/partials/ModalDownload.html'],
