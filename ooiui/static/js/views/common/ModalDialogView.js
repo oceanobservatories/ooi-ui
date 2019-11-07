dc07b211aab9bf562e5d701a3755799365b8b4e3
oceanzus <case@oceanz.org>
2019-11-07 16:17:10 -0500

    Adds notification banner for link back to oceanobservatories.org.

--- ooiui/static/js/views/common/ModalDialogView.js
+++ ooiui/static/js/views/common/ModalDialogView.js
@@ -15,7 +15,8 @@
 var ModalDialogView = Backbone.View.extend({
   className: 'modal fade',
   events: {
-    'hidden.bs.modal' : 'hidden'
+    'hidden.bs.modal' : 'hidden',
+    'click #dataupdate' : 'setDataUpdateCookie'
   },
   hide: function() {
     this.$el.modal('hide');
@@ -24,6 +25,23 @@ var ModalDialogView = Backbone.View.extend({
     if(this.ack) {
       this.ack();
     }
+  },
+  setDataUpdateCookie: function(e) {
+    console.log(e);
+
+    var date = new Date();
+    date.setTime(date.getTime() + 12500*1000);
+
+    if(e.target.checked === true){
+      console.log('checked');
+      Cookies.set('datanotification', 'hide', {expires: date, path: '/'});
+      console.log(Cookies.get('datanotification'));
+    } else {
+      console.log('unchecked');
+      Cookies.set('datanotification', 'show', {expires: date, path: '/'});
+      console.log(Cookies.get('datanotification'));
+    }
+
   },
   initialize: function() {
     _.bindAll(this, "render", "show", "hidden");
